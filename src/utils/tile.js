1d7063aa3630c801ec23f6ca7ffe2c072b8e7bd7
comarasj <comarasj@mail.uc.edu>
2019-02-20 13:21:20 -0500

    added tile util

---
+++ src/utils/tile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './tile.css';
+class Tile extends Component {
+    render() {
+        return (
+            <div>
+                Tile
+            </div>
+        )
+    }
+}
+
+export default Tile; 
